45d496db076932dafa7d7029205eea72d50e0696
rw-r-r-0644 <r.r.qwertyuiop.r.r@gmail.com>
2021-06-01 13:20:55 +0200

    isfshax: ensure correct superblock alignment

--- stage2/isfs/isfshax.h
+++ stage2/isfs/isfshax.h
@@ -38,7 +38,7 @@ typedef struct isfshax_super
     u16 fat[CLUSTER_COUNT];
     isfs_fst fst[6143];
     isfshax_info isfshax;
-} PACKED ALIGNED(64) isfshax_super;
+} PACKED ALIGNED(NAND_DATA_ALIGN) isfshax_super;
 
 _Static_assert(sizeof(isfshax_super) == ISFSSUPER_SIZE, "isfshax_super must be 0x40000");
 
